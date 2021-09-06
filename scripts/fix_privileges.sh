c276b193930c80fe1f052eeb9be45037ee67bb55
facubarberis <facubarberis@gmail.com>
2021-09-06 09:14:45 -0300

    Revert "NuevaWeb"
    This reverts commit e585810abfc3ca7da85d829e67d5d91508a65dc4.

---
+++ scripts/fix_privileges.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+chmod +x /home/ec2-user/AplicacionCodeDeploy/scripts/* jar
+chmod +x /home/ec2-user/AplicacionCodeDeploy/scripts/server_start.sh
+chmod +x /home/ec2-user/AplicacionCodeDeploy/scripts/server_stop.sh

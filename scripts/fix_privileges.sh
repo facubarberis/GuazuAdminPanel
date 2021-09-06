e585810abfc3ca7da85d829e67d5d91508a65dc4
facubarberis <facubarberis@gmail.com>
2021-09-06 09:07:03 -0300

    NuevaWeb

--- scripts/fix_privileges.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-chmod +x /home/ec2-user/AplicacionCodeDeploy/scripts/* jar
-chmod +x /home/ec2-user/AplicacionCodeDeploy/scripts/server_start.sh
-chmod +x /home/ec2-user/AplicacionCodeDeploy/scripts/server_stop.sh
